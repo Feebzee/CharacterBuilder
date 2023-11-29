ed8cc14194f055289fb6efa6aaec1ddd7a18e89a
Feebzee <144839414+Feebzee@users.noreply.github.com>
2023-11-29 13:07:58 -0500

    Update BuilderGUI.java

--- BuilderGUI.java
+++ BuilderGUI.java
@@ -1,84 +1,92 @@
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class BuilderGUI extends Application {
     public static void main(String[] args) {
-        Application.launch();
+        launch();
     }
     @Override
     public void start(Stage stage){
-        Character myChar = new Character();
         ArrayList<Character> myChars = new ArrayList<>();
         BorderPane borderPane = new BorderPane();
-        HBox buttons = new HBox();
+        GridPane[] grid = new GridPane[2];
+        Button[] switchGrids = new Button[2];
+        HBox[] hBoxes = new HBox[3];
+        VBox[] vBoxes  = new VBox[2];
 
-        Button buildChar = new Button("New Character");
-        buildChar.setOnAction(actionEvent ->{
-            buildChar(borderPane, myChar, myChars);
+        //setting the scene
+        borderPane.setTop(hBoxes[0]);
+        borderPane.setCenter(grid[0]);
+        borderPane.setRight(ReferenceFiles.displayTypes());
+
+        //save characters to a new file
+        Button saveToFile = new Button("Save characters");
+        saveToFile.setOnAction(actionEvent -> {
+            ReferenceFiles.saveCharsToFile(Character.buildMap(myChars));
         });
-        buttons.getChildren().add(buildChar);
+        hBoxes[0].getChildren().addAll(saveToFile);
+        hBoxes[1].getChildren().addAll(saveToFile);
 
-        Button seeChars = new Button("View Characters");
-        seeChars.setOnAction(actionEvent -> {
-            VBox view = new VBox();
+        //for building characters, setting grid/hboxes[0]
+        switchGrids[0] = new Button("View characters");
+        Button buildCharMan = new Button("Build new character");
+        Button buildCharRand = new Button("Add random character");
+        hBoxes[0].getChildren().addAll(buildCharMan,buildCharRand,switchGrids[0]);
+        switchGrids[0].setOnAction(actionEvent -> {
+            borderPane.getChildren().clear();
+            borderPane.setTop(hBoxes[1]);
+            borderPane.setCenter(grid[1]);
+        });
+        buildCharMan.setOnAction(actionEvent ->{
+            grid[0].getChildren().clear();
+            grid[0] = ReferenceFiles.buildChar(false,myChars);
+        });
+        buildCharRand.setOnAction(actionEvent ->{
+            grid[0].getChildren().clear();
+            grid[0] = ReferenceFiles.buildChar(true,myChars);
+        });
+
+        //for viewing characters and the group's overall power set,
+        //setting grid/hboxes[1]
+        switchGrids[1] = new Button("Add more characters");
+        Button seeAChar = new Button("View 1 Character");
+        Button seeAllChars = new Button("View all Characters");
+        Button displayCharsPowerSet = new Button("See your team's overall power set");
+        hBoxes[1].getChildren().addAll(seeAChar,seeAllChars,
+                displayCharsPowerSet,switchGrids[1]);
+        switchGrids[1].setOnAction(actionEvent -> {
+            borderPane.getChildren().clear();
+            borderPane.setTop(hBoxes[0]);
+            borderPane.setCenter(grid[0]);
+        });
+        seeAChar.setOnAction(actionEvent -> {
             for(int i=0; i< myChars.size(); i++) {
-                view.getChildren().add(new Label(myChars.get(i).getName()));
+                hBoxes[2] = ReferenceFiles.displayChar(myChars);
+                grid[1].add(hBoxes[2],1,0,2,1);
             }
-            borderPane.setCenter(view);
         });
-        buttons.getChildren().add(seeChars);
-        borderPane.setTop(buttons);
+        seeAllChars.setOnAction(actionEvent -> {
+            for(int i=0; i< myChars.size(); i++) {
+                vBoxes[0] = ReferenceFiles.displayAllChars(myChars);
+                grid[1].add(vBoxes[0],3,0,1,2);
+            }
+        });
+        Hashtable<Power,Integer> table = Power.buildHashtable(myChars);
+        displayCharsPowerSet.setOnAction(actionEvent -> {
+            vBoxes[1] = ReferenceFiles.displayPowerSet(table);
+            grid[1].add(vBoxes[1],1,1,1,2);
+        });
 
         Scene scene = new Scene(borderPane);
         stage.setScene(scene);
-    }
-
-    public static void buildChar(BorderPane borderPane, Character myChar, ArrayList<Character> myChars){
-        GridPane grid = new GridPane(100,100);
-        HBox displayChar = new HBox();
-
-        grid.setGridLinesVisible(true);
-        grid.add(new Label("Name: "), 0,0);
-        TextField nameField = new TextField();
-        grid.add(nameField, 1,0);
-        myChar.setName(nameField.getText());
-
-        TextField ageField = new TextField();
-        grid.add(new Label("Age: "), 0,1);
-        grid.add(ageField, 1,1);
-        myChar.setAge(Integer.parseInt(ageField.getText()));
-
-        TextField speciesField = new TextField();
-        grid.add(new Label("Species: "), 0,2);
-        grid.add(speciesField, 1,2);
-        myChar.setSpecies(new Species(speciesField.getText()));
-
-        grid.add(new Label("Power (type left, level right): "), 0,3);
-        TextField powerNameField = new TextField();
-        TextField powerLevelField = new TextField();
-        grid.add(powerNameField, 1,3);
-        grid.add(powerLevelField, 2,3);
-        myChar.setPower(new Power(powerNameField.getText(),
-                Integer.parseInt(powerLevelField.getText()),
-                myChar.getSpecies().getSpeciesName()));
-        displayChar.getChildren().add(new Label(myChar.toString()));
-
-        borderPane.setCenter(grid);
-        borderPane.setBottom(displayChar);
-        myChars.add(myChar);
-
-        Scene scene = new Scene(borderPane);
     }
 }
